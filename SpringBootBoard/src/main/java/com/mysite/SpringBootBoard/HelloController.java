39adc0f7810912913076b2bcaee50ba32f8ffb07
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-11-20 20:00:05 +0900

    [create]: 신규프로젝트 생성
    신규 프로젝트 생성, HelloController 추가

---
+++ SpringBootBoard/src/main/java/com/mysite/SpringBootBoard/HelloController.java
@@ -0,0 +1,14 @@
+package com.mysite.SpringBootBoard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody   //hello 메서드의 응답 결과가 문자열 그 자체. 문자열을 return
+    public String hello() {
+        return "Hello World";
+    }
+}
