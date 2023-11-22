845df21f9976833ba95940d9bad79567fabb3e7a
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-11-22 14:20:29 +0900

    [update]: 롬복수정

--- SpringBootBoard/src/main/java/com/mysite/SpringBootBoard/HelloLombok.java
+++ SpringBootBoard/src/main/java/com/mysite/SpringBootBoard/HelloLombok.java
@@ -6,7 +6,8 @@ import lombok.Setter;
 
 //2) @RequiredArgsConstructor
 @Getter
-//1) @Setter
+//1)@Setter
+//@Data
 public class HelloLombok {
 
     //1) Getter, Setter 어노테이션 이용 시
@@ -17,14 +18,15 @@ public class HelloLombok {
     private final int lombok;
 
 
-    //2) @RequiredArgsConstructor 대신 3) 생성자를 직접 작성할 수 있다. 의존성 주입(Dependency Injection)시 사용된다.
+    //2) @RequiredArgsConstructor 대신
+    //3) 생성자를 직접 작성할 수 있다. 의존성 주입(Dependency Injection)시 사용된다.
     public HelloLombok(String hello, int lombok) {
         this.hello = hello;
         this.lombok = lombok;
     }
 
     public static void main(String[] args) {
-//        1) Getter, Setter 어노테이션 이용 시
+//       1) Getter, Setter 어노테이션 이용 시
 //        HelloLombok helloLombok = new HelloLombok();
 //        helloLombok.setHello("헬로");
 //        helloLombok.setLombok(5);
@@ -32,8 +34,11 @@ public class HelloLombok {
         //2), 3) 작성 시
         HelloLombok helloLombok = new HelloLombok("헬로", 5);
 
+
         System.out.println(helloLombok.getHello());
         System.out.println(helloLombok.getLombok());
+
+
 
     }
 }
